e04397ba6928c3b20009d5330d5329e46da50510
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2024-11-08 21:07:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-<script>
-    window.addEventListener("scroll", function() {
-        const footer = document.querySelector("footer");
-
-        if (window.scrollY > 100) { // Show after scrolling down a bit
-            footer.classList.add("visible");
-        } else {
-            footer.classList.remove("visible");
-        }
-    });
-</script>#
+#

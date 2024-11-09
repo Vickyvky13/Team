b1f39dd07a38ef87994b22c499f8c4d5f8113358
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2024-11-09 13:06:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-#
+function toggleSocial(card) {
+    // Hide all social containers
+    const allSocialContainers = document.querySelectorAll('.social-container');
+    allSocialContainers.forEach(container => {
+        container.classList.add('hidden');
+    });
+
+    // Show the social container for the clicked card
+    const socialContainer = card.querySelector('.social-container');
+    socialContainer.classList.toggle('hidden');
+
+    // Automatically hide the shown social container after 3 seconds
+    setTimeout(() => {
+        socialContainer.classList.add('hidden');
+    }, 3000);
+}

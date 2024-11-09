78605d20c5fcd5f713b0fba52b9d89faf3b081a4
Vickyvky13 <131648662+Vickyvky13@users.noreply.github.com>
2024-11-09 13:11:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 function toggleSocial(card) {
-    // Hide all social containers
-    const allSocialContainers = document.querySelectorAll('.social-container');
-    allSocialContainers.forEach(container => {
-        container.classList.add('hidden');
+    // First, hide all other social containers
+    document.querySelectorAll('.social-container').forEach(container => {
+        if (container !== card.querySelector('.social-container')) {
+            container.classList.add('hidden');
+        }
     });
 
-    // Show the social container for the clicked card
+    // Then toggle the clicked card's social container
     const socialContainer = card.querySelector('.social-container');
     socialContainer.classList.toggle('hidden');
 
-    // Automatically hide the shown social container after 3 seconds
+    // Automatically hide the social container after 3 seconds
     setTimeout(() => {
         socialContainer.classList.add('hidden');
     }, 3000);
